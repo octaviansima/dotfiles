5c5ca70836f8942d962d00364969be4ff8d8b04a
Octavian Sima <octavian.sima98@gmail.com>
2024-02-28 11:55:33 -0800

    changes

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -eo pipefail
+
+if [ "$(uname)" != "Darwin" ]; then
+    echo "Script not designed to run on $(uname)"
+    exit 1
+fi
+
+sudo cp .gitconfig ~/
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+brew update
+brew install neovim
+
+mkdir -p ~/.config
+mkdir -p ~/.config/nvim
+sudo cp init.vim ~/.config/nvim/init.vim
+
+brew install tmux
+brew install reattach-to-user-namespace
+
+brew install starship
+
+sudo cp .tmux.conf ~/
+sudo cp .tmux.theme ~/
+
+sudo cp .profile ~/
+
